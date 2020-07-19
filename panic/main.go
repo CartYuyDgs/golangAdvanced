6eb7a1d24bab2e9bd36eec37f3d06d9b724b5ce5
于洋 <yy_dgs@126.com>
2020-07-19 23:45:24 +0800

    recover

--- panic/main.go
+++ panic/main.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	defer func() {
-		message := recover()
+	//defer func() {
+	//	message := recover()
+	//	fmt.Println(message)
+	//}()
+	defer receiveRecover()
+	//panic("i am panic")
+	//panic(errors.New("i am error"))
+	panic(1)
+}
+
+func receiveRecover() {
+	message := recover()
+	fmt.Println(message)
+	switch message.(type) {
+	case string:
+		fmt.Println("string message", message)
+	case error:
+		fmt.Println("err: ", message)
+	default:
 		fmt.Println(message)
-	}()
-	panic("i am panic")
+	}
 }
