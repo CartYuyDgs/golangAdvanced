5a83d89fac3098c839840361983047c6f7556d34
于洋 <yy_dgs@126.com>
2020-07-28 22:50:25 +0800

    生产者多消费者

--- channel/chanmod.go
+++ channel/chanmod.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Ticker struct {
 	Name  string
@@ -11,7 +14,7 @@ type Vector struct {
 	Data chan Ticker
 }
 
-func main() {
+func demo1() {
 	v := &Vector{Data: make(chan Ticker, 20)}
 
 	for i := 0; i < 20; i++ {
@@ -36,4 +39,55 @@ func main() {
 	}(v)
 
 	<-done
+}
+
+func demo2() {
+	v := &Vector{Data: make(chan Ticker, 1)}
+
+	go func(vector *Vector) {
+		for {
+			if v, ok := <-vector.Data; ok {
+				fmt.Println(v.Name, v.Price)
+			}
+		}
+	}(v)
+
+	for {
+		v.Data <- Ticker{
+			Name:  "vector",
+			Price: 20,
+		}
+	}
+}
+
+func main() {
+	v := &Vector{Data: make(chan Ticker, 100)}
+	//生产
+	for j := 0; j < 100; j++ {
+		v.Data <- Ticker{
+			Name:  "vector",
+			Price: j,
+		}
+	}
+
+	//消费
+	wg := new(sync.WaitGroup)
+	for k := 0; k < 100; k++ {
+		wg.Add(1)
+		go func(vector *Vector) {
+			for {
+				if len(vector.Data) == 0 {
+					wg.Done()
+					fmt.Println("，没有票了。。。。")
+					return
+				}
+				if v, ok := <-vector.Data; ok {
+					fmt.Println(v.Price, v.Name)
+				}
+			}
+
+		}(v)
+
+	}
+	wg.Wait()
 }
